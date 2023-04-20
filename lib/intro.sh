eec884c44227eabaf1bcc017878e56c0ce38602d
Kay Lack <kay@makers.tech>
2023-04-20 20:34:35 +0100

    [SLUG] Transferring changes from Slug as of 2023-04-20 20:34:30 +0100.

--- lib/intro.sh
+++ lib/intro.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 BLUE='\033[1;34m'
+RED='\033[1;31m'
 NC='\033[0m'
 
-echo -e "${BLUE}This is your shell."
-echo "You use it to run programs."
+echo -e "${RED}If you're seeing scrolling text on the right,"
+echo "  you're in the wrong place."
+echo -e "Hit the 'Fork' button to get started.${NC}"
+echo
+echo -e "${BLUE}Otherwise: welcome home."
+echo "This is your shell."
+echo "You can use it to run Python programs."
 echo
 echo "If you ever lose it, hit the big green 'Run' button."
 echo
